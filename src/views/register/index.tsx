d72f9c4e8155510ebe94c671d79c4e6875cbf1f6
caizhihao <177745994@qq.com>
2023-05-23 20:34:18 +0800

    环境

---
+++ src/views/register/index.tsx
@@ -0,0 +1,13 @@
+/*
+ * @Author: caizhihao
+ * @Date: 2023-05-23 20:24:35
+ * @LastEditors: caizhihao 177745994@qq.com
+ * @LastEditTime: 2023-05-23 20:30:15
+ * @FilePath: \react\react-cocashy-pay\src\views\register\index.tsx
+ * @Description:
+ *
+ */
+
+export const Register = () => {
+	return <div>11111</div>
+}
